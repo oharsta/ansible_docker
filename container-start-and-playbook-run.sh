b6dbf8fbebdf56f360ce80de130904751e81a274
Okke Harsta <oharsta@zilverline.com>
2019-03-08 10:38:36 +0100

    WIP:

--- container-start-and-playbook-run.sh
+++ container-start-and-playbook-run.sh
@@ -6,7 +6,7 @@ cd docker && docker build -t myubuntu .
 
 docker run -ti --privileged --name $DOCKER_CONTAINER_NAME -d -p 5000:22 -p 80:80 -p 443:443 myubuntu
 
-cd ../ansible && ansible-playbook -i env/local_docker myplaybook.yml
+cd ../ansible && ansible-playbook -i env/local_docker playbook.yml
 # docker stop $DOCKER_CONTAINER_NAME
 #
 # docker rm $DOCKER_CONTAINER_NAME
